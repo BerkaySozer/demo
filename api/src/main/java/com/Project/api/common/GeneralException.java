59517353d58480630e30b5c01b65624064968460
BerkaySozer <83730857+BerkaySozer@users.noreply.github.com>
2023-07-27 17:18:33 +0300

    Add files via upload
    Docker Enabled JSpringBoot Backend

---
+++ api/src/main/java/com/Project/api/common/GeneralException.java
@@ -0,0 +1,7 @@
+package com.Project.api.common;
+
+public class GeneralException extends RuntimeException{
+    public GeneralException(String message) {
+        super(message);
+    }
+}
---
+++ api/src/main/java/com/Project/api/entity/enums/Role.java
@@ -0,0 +1,6 @@
+package com.Project.api.entity.enums;
+
+public enum Role {
+    STUDENT,
+    TEACHER
+}
